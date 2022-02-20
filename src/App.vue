76f02abeccde6d2860b522b1e1c00ae107c47129
AleksandrovArtyom <artyomik14@gmail.com>
2022-02-20 11:52:03 +0300

    Speedometer done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<Speedometer :speed="speed" :maxSpeed="maxSpeed" :maxFuel="maxFuel" :fuel="fuel" :isDoorsOpen="isDoorsOpen" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Speedometer from "./components/Speedometer.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		Speedometer,
+	},
+    data: function() {
+        return {
+            speed: 0,
+            maxSpeed: 360,
+            fuel: 100,
+            maxFuel: 100,
+            isDoorsOpen: true,
+        }
+    },
+    mounted: function() {
+        let speedSign = 1;
+        let fuelSign = -1;
+        setInterval(() => {
+            this.speed += 1 * speedSign;
+            if((this.speed > this.maxSpeed && speedSign > 0) || (this.speed < 0 && speedSign < 0)) {
+                speedSign *= -1;
+            }
+
+            this.fuel += 1 * fuelSign;
+            if(this.fuel > this.maxFuel && fuelSign > 0 || this.fuel < 0 && fuelSign < 0) {
+                fuelSign *= -1;
+            }
+        }, 10)
+    }
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+	background-color: black;
 }
 </style>
